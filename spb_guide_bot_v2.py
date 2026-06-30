9bbed3f2b72aed0c6bab9fb78ccc577e105b0cc7
mariamichshenko9577-coder <mariamichshenko9577@gmail.com>
2026-07-01 00:19:15 +0300

    Update spb_guide_bot_v2.py

--- spb_guide_bot_v2.py
+++ spb_guide_bot_v2.py
@@ -8,7 +8,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
 
 BOT_TOKEN   = "8874441025:AAEAwTWU8J8LaUTf78fKqn1StUnrH1O30Ec"
-PAYMENT_URL = "@Masha_Mich"
+PAYMENT_URL = "https://t.me/Masha_Mich"
 GUIDE_PRICE = "790 ₽"
 AUTHOR_NAME = "Мария"
 
